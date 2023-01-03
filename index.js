1266feddf83a08be45a43e8bdbba33cb5a2a1b1d
Ahmet <40005914+ahmetkosker@users.noreply.github.com>
2023-01-03 15:57:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 var cors = require("cors");
-const https = require("https");
+const https = require("http");
 
 const app = express();
 const { Server } = require("socket.io");
